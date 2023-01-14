4420c88139f28d9921f29546969cba58e421c81a
akashkalam <akashkalam1020@gmail.com>
2023-01-14 20:10:17 +0530

    Data Validation

---
+++ main.py
@@ -0,0 +1,25 @@
+from cement.logger import logging
+from cement.exception import CSException
+from cement.utils import get_collection_as_dataframe
+import sys,os
+from cement.entity import config_entity
+from cement.components.data_ingestion import DataIngestion
+from cement.components.data_validation import DataValidation
+
+print(__name__)
+if __name__=="__main__":
+    try:
+        training_pipeline_config = config_entity.TrainingPipelineConfig()
+        data_ingestion_config  = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+        print(data_ingestion_config.to_dict())
+        data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+        data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+        
+        #data validation
+        data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
+        data_validation = DataValidation(data_validation_config=data_validation_config,
+                        data_ingestion_artifact=data_ingestion_artifact)
+
+        data_validation_artifact = data_validation.initiate_data_validation()
+    except Exception as e:
+        print(e)
