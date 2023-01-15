5160396beaafcfe520a4540cf30f55e02a8cf0e3
akashkalam <akashkalam1020@gmail.com>
2023-01-15 21:21:48 +0530

    Project Completed

--- cement/entity/artifact_entity.py
+++ cement/entity/artifact_entity.py
@@ -16,13 +16,13 @@ class DataTransformationArtifact:
     transform_object_path:str
     transformed_train_path:str
     transformed_test_path:str
-    target_encoder_path:str
+#    target_encoder_path:str
 
 @dataclass
 class ModelTrainerArtifact:
     model_path:str 
-    f1_train_score:float 
-    f1_test_score:float
+    r2_train_score:float 
+    r2_test_score:float
 
 @dataclass
 class ModelEvaluationArtifact:
